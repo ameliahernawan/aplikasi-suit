efd4e3a9a9961700643984df903e675649a1effc
Brilliant FRiezka Aina <brilliant.friezka@gmail.com>
2024-12-21 13:31:06 +0700

    coba

--- screens/HomePage.jsx
+++ screens/HomePage.jsx
@@ -4,7 +4,6 @@ export default function HomePage() {
   return (
     <View style={{ flex: 1, justifyContent: 'flex-end', gap: 12, margin: 10 }}>
       <Text>Ini home page</Text>
-      <Text>Ini home page</Text>
     </View>
   );
 }
